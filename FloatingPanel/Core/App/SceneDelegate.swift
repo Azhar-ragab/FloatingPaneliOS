8a74317aa420962337ca37d567b4a16b1d3d647b
AzharRagab <azharragab9@gmail.com>
2023-02-06 19:51:23 +0200

    remove main storyBoard

--- FloatingPanel/Core/App/SceneDelegate.swift
+++ FloatingPanel/Core/App/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        let vc = ViewController()
+        let vc = BaseViewController()
         RootRouter.presentRootScreen(in: window, vc: vc)
     }
 
